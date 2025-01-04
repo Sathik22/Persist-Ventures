9d0268d6c51473649f744b4f3179d00111e97f9a
Sathik22 <mohamedjahabarsathik2001@gmail.com>
2025-01-04 15:59:55 +0530

    changed file name

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
           <ul className="hidden lg:flex ml-14 space-x-12">
             {navItems.map((item, index) => (
               <li key={index}>
-                <a href={item.href}>{item.label}</a>
+                <a href={item.href}>{item.label}</a>   
               </li>
             ))}
           </ul>
